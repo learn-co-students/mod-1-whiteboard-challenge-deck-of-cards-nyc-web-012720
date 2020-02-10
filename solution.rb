7a73cfbfd1084bf5e83128e8cbba893062c40cb3
davidmlee4 <davidmlee4@gmail.com>
2020-02-10 10:01:58 -0500

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,45 @@
+require 'pry'
+
 class Deck
+    attr_accessor :cards
+
+    RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    SUITS = ["Hearts", "Clubs", "Diamonds", "Spades"]
+
+    def initialize()
+        @cards = []
+
+        SUITS.each{|suit|
+            RANKS.each{|rank|
+            @cards << Card.new(suit,rank)
+            }
+        }
+
+    end
+
+    def shuffle
+        @cards.shuffle
+    end
+
+    def choose_card
+        @cards.delete(@cards[rand(0..51)])
+    end
+
 
 end
 
 class Card
+    
+    attr_accessor :rank, :suit
+
+    def initialize(suit,rank)
+        @rank = rank
+        @suit = suit
+    end
 
 end
+
+a = Deck.new
+# binding.pry
+
+#hello
